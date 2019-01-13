b9fb7f574e450d31be8e0d3f12075ca9905ea3f0
Chitipat Thabsuwan <chitipat.toy@gmail.com>
2019-01-13 16:48:16 +0700

    edit Logic

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
+const path = require("path");
 const line = require("@line/bot-sdk");
 const app = express();
-const firebaseService = require('./firebase-service');
+const firebaseService = require("./firebase-service");
 
 app.set("port", process.env.PORT || 5000);
 // Process application/json
 app.use(bodyParser.json());
 
-process.env.CHANNEL_ACCESS_TOKEN = "LprJ2or3L5+2hOn4qkZPv/s3XNff2q08+kDRe8oWvb3UpBKJudvNCPgdstxQZIT7G0qwRyY39PjI2E/MJzTa1teQj19mE6QfQpQZZwDdNxn6ESWGbl2AMhYHRq+bP4u8z5US28J2MD9+mTZPGHmHWQdB04t89/1O/w1cDnyilFU="
+process.env.CHANNEL_ACCESS_TOKEN =
+  "LprJ2or3L5+2hOn4qkZPv/s3XNff2q08+kDRe8oWvb3UpBKJudvNCPgdstxQZIT7G0qwRyY39PjI2E/MJzTa1teQj19mE6QfQpQZZwDdNxn6ESWGbl2AMhYHRq+bP4u8z5US28J2MD9+mTZPGHmHWQdB04t89/1O/w1cDnyilFU=";
 
 const client = new line.Client({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
@@ -22,10 +23,12 @@ const message = {
   text: "Hello World!"
 };
 
-let _messages = [{
-  type: 'text',
-  text: message
-}];
+let _messages = [
+  {
+    type: "text",
+    text: message
+  }
+];
 
 app.post("/webhook", (req, res) => {
   var text = req.body.events[0].message.text;
@@ -37,46 +40,46 @@ app.post("/webhook", (req, res) => {
     _messages[0].text = tt;
     console.log(tt);
     client
-    .replyMessage(replyToken, _messages)
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      // error handling
-    });
-  })
+      .replyMessage(replyToken, _messages)
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(err => {
+        // error handling
+      });
+  });
 
-  var arr_str = text.split(" : ");
-  var name = arr_str[0];
-  var date = arr_str[1];
-  var time = "12:00"
-  if(arr_str.length == 3){
-    time = arr_str[2];
-  }
-
-  var taskId = new Date().valueOf();
-
-  firebaseService.addTask(taskId, name, date, time);
-
-  if(text == "edit"){
+  if (text == "edit") {
     client
-    .replyMessage(replyToken, "https://to-do-dev.herokuapp.com/")
-    .then(() => {
-      res.sendStatus(200);
-    })
-    .catch(err => {
-      // error handling
-    });
+      .replyMessage(replyToken, "https://to-do-dev.herokuapp.com/")
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch(err => {
+        // error handling
+      });
+  } else {
+    var arr_str = text.split(" : ");
+    var name = arr_str[0];
+    var date = arr_str[1];
+    var time = "12:00";
+    if (arr_str.length == 3) {
+      time = arr_str[2];
+    }
 
+    var taskId = new Date().valueOf();
+    firebaseService.addTask(taskId, name, date, time);
   }
 });
 
 app.get("/", (req, res) => {
-  res.redirect('https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1638410516&redirect_uri=https://to-do-dev.herokuapp.com/edit&state=12345abcde&scope=openid');
-})
+  res.redirect(
+    "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1638410516&redirect_uri=https://to-do-dev.herokuapp.com/edit&state=12345abcde&scope=openid"
+  );
+});
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'index.html'));
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
 });
 
 // Spin up the server
